a7bcbd8484abbf2ec89c1b01e12c6192e029516f
Burcugozluklu <81200212+Burcugozluklu@users.noreply.github.com>
2023-04-10 16:02:50 -0400

    set attribute

--- src/app.js
+++ src/app.js
@@ -20,15 +20,22 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let icon.Element =document.querySelector("#icon")
   temperatureElement.innerHTML = HTML.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = "Friday 5:00";
+  iconElement.setAttribute(
+    "src" ,
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
 console.log(apiUrl);
 axios.get(url).then(displayTemperature);
