9397d9a3f462b18ab9315a773968e32f524eae3a
Burcugozluklu <81200212+Burcugozluklu@users.noreply.github.com>
2023-04-10 16:35:18 -0400

    search engine

--- src/app.js
+++ src/app.js
@@ -33,9 +33,20 @@ function displayTemperature(response) {
     );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-console.log(apiUrl);
-axios.get(url).then(displayTemperature);
+function search(city) {
+ let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+ axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event){
+   event.preventDefault();
+   let cityInputElement = document.querySelector("#city-input");
+   search(cityInputElement.value);
+   console.log(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
