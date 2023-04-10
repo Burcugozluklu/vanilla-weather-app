21613cb16c9a1505ef4bc0f3ec35eb5b06195175
Burcugozluklu <81200212+Burcugozluklu@users.noreply.github.com>
2023-04-10 14:45:30 -0400

    updated time

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
+function formatDate(timestamp){
+  let date = new Date(timestamp);
+  let hours = timestamp.getHours();
+  if(hours < 10){
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if(minutes < 10){
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = date.getDay();
+  return `${day} ${hours} ${minutes}`;
+}
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let windElement.innerHTML = response.data.wind.speed;
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = HTML.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = "Friday 5:00";
 }
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
